721eb8fdc357fe459ab1103c10efdbdd31b8aec3
BohdanOmelianets <omelianecb@gmail.com>
2023-11-05 22:09:47 +0200

    update contact section

--- src/components/Contants/Contacts.styled.tsx
+++ src/components/Contants/Contacts.styled.tsx
@@ -1,8 +1,92 @@
 import styled from "styled-components";
 
-export const HeroTitle = styled.h2`
-  background: var(--gradient);
-  -webkit-background-clip: text;
-  color: transparent;
+export const ImageBackground = styled.img`
+  position: absolute;
+  // inset: 0;
+  top: 20%;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  mask-image: radial-gradient(
+    circle at bottom,
+    rgb(0, 0, 0, 0.5) 10%,
+    transparent 65%
+  );
+  -webkit-mask-image: radial-gradient(
+    circle at bottom,
+    rgb(0, 0, 0, 0.5) 10%,
+    transparent 65%
+  );
+  z-index: -1;
+`;
+
+export const Content = styled.div`
+  height: 100%;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  gap: 32px;
+`;
+
+export const Paragraph = styled.p`
+  width: clamp(300px, 100%, 400px);
+  letter-spacing: 2px;
+  font-size: 24px;
+  padding: 16px;
+`;
+
+export const DivStyled = styled.div`
+  width: clamp(300px, 100%, 400px);
+  background: var(--darkStaleBlueTr);
+  border-radius: 20px;
+  font-size: 18px;
+  padding: 16px;
+`;
+
+export const ContactItem = styled.div`
+  padding: 16px;
+
+  &.flex {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 16px;
+  }
+`;
+
+export const ContactTitle = styled.span`
+  display: block;
+  font-family: GabaritoMedium;
+  margin-bottom: 8px;
+`;
+
+export const ContactText = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 8px;
+
+  font-size: 16px;
+  font-family: GabaritoRegular;
+  color: #9d9d9d;
+
+  transition: color 0.3s;
+
+  &:hover {
+    color: var(--accentPink);
+  }
+`;
+
+export const SvgIcon = styled.span`
   display: inline-block;
+  width: 30px;
+  height: 30px;
+
+  & svg {
+    transition: all 0.4s;
+  }
+
+  &:hover svg {
+    transform: scale(1.1);
+    fill: var(--accentPink);
+  }
 `;
--- src/components/Contants/index.tsx
+++ src/components/Contants/index.tsx
@@ -1,114 +1,98 @@
-
-import styled from "styled-components";
-// import {
-  
-// } from "./Contacts.styled";
+import {
+  ImageBackground,
+  Content,
+  Paragraph,
+  DivStyled,
+  ContactItem,
+  ContactTitle,
+  ContactText,
+  SvgIcon,
+} from "./Contacts.styled";
 import { Container } from "components/Container/Container.styled";
 import { SectionTitle } from "components/ui/SectionTitle";
-
-
-
-
-const ImageBackground = styled.img`
-  position: absolute;
-  // inset: 0;
-  top: 20%;
-  width: 100%;
-  height: 100%;
-  object-fit: cover;
-  mask-image: radial-gradient(circle at bottom, rgb(0,0,0,.5) 10%, transparent 65%);
-  -webkit-mask-image: radial-gradient(circle at bottom, rgb(0,0,0,.5) 10%, transparent 65%);
-  z-index: -1;
-`;
-
-const Content = styled.div`
-  height: 100%;
-  display: grid;
-  grid-template-columns: repeat(2, 1fr);
-  // border: 1px solid orange;
-  justify-items: center;
-  align-items: center;
-`;
-
-const Paragraph = styled.p`
-  width: 85%;
-  letter-spacing: 2px;
-  font-size: 24px;
-  padding: 16px;
-`;
-
-const Links = styled.div`
-  height: 100%;
-  // background: var(--darkStaleBlueTr);
-  // border-radius: 20px;
-  // width: 70%;
-  font-size: 18px;
-  padding: 16px;
-  display: grid;
-  grid-template-columns: repeat(2, 1fr);
-  grid-template-rows: 1fr;
-  gap: 32px;
-`;
-
-const LinkItem = styled.div`
-  height: 100%;
-  // background: var(--darkStaleBlueTr);
-  border-radius: 20px;
-  // width: 70%;
-  font-size: 18px;
-  padding: 16px;
-
-  &:nth-child(3) {
-    grid-column: 1/3;
-  }
-
-  &.flex {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    gap: 16px;
-  }
-
-`;
-
-const SocialIcon = styled.a`
-  display: inline-block;
-  width: 50px;
-  height: 50px;
-  & img {
-    width: 100%;
-    height: 100%;
-    object-fit: cover;
-  }
-
-`;
+import {
+  EmailSVG,
+  FacebookSVG,
+  InstagramSVG,
+  LinkedinSVG,
+  LocationSVG,
+  PhoneSVG,
+} from "./svgs";
 
 
 const Contacts = () => {
   return (
     <Container as="section">
       <ImageBackground src="/images/contacts_bg.jpg" />
-      <SectionTitle className="title">
-        Let's keep in touch
-      </SectionTitle>
+      <SectionTitle className="title">Let's keep in touch</SectionTitle>
       <Content>
         <Paragraph>
-          Feel free to get in touch with me for any inquiries, collaborations, or just to say hello.
-          I'm always open to connecting with new people and exploring new opportunities.
+          Feel free to get in touch with me for any inquiries, collaborations,
+          or just to say hello. I'm always open to connecting with new people
+          and exploring new opportunities.
           {/* You can reach me through the following methods: */}
         </Paragraph>
-        <Links>
-          <LinkItem>Email: omelianecb@gmail.com</LinkItem>
-          <LinkItem>Phone: 0630669064</LinkItem>
-          <LinkItem className="flex">
-            <SocialIcon><img src="/images/contacts/facebook.png" width="100" alt="" /></SocialIcon>
-            <SocialIcon><img src="/images/contacts/instagram.png" width="100" alt="" /></SocialIcon>
-            <SocialIcon><img src="/images/contacts/linkedin.png" width="100" alt="" /></SocialIcon>
-          </LinkItem>
-        </Links>
+        <DivStyled>
+          <ContactItem>
+            <ContactTitle>Address:</ContactTitle>
+            <ContactText
+              as="a"
+              href="https://www.google.com/maps/place/%D0%9B%D1%8C%D0%B2%D1%96%D0%B2,+%D0%9B%D1%8C%D0%B2%D1%96%D0%B2%D1%81%D1%8C%D0%BA%D0%B0+%D0%BE%D0%B1%D0%BB%D0%B0%D1%81%D1%82%D1%8C,+79000/@49.8327706,23.9298348,12z/data=!3m1!4b1!4m6!3m5!1s0x473add7c09109a57:0x4223c517012378e2!8m2!3d49.839683!4d24.029717!16zL20vMGQ3X24?entry=ttu"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <SvgIcon>
+                <LocationSVG />
+              </SvgIcon>
+              <span>Ukraine, Lviv</span>
+            </ContactText>
+          </ContactItem>
 
+          <ContactItem>
+            <ContactTitle>Email:</ContactTitle>
+            <ContactText as="a" href="mailto:omelianecb@gmail.com">
+              <SvgIcon>
+                <EmailSVG />
+              </SvgIcon>
+              <span>omelianecb@gmail.com</span>
+            </ContactText>
+          </ContactItem>
+
+          <ContactItem>
+            <ContactTitle>Phone:</ContactTitle>
+            <ContactText as="a" href="tel:+380630669064">
+              <SvgIcon>
+                <PhoneSVG />
+              </SvgIcon>
+              <span>+380630669064</span>
+            </ContactText>
+          </ContactItem>
+
+          <ContactItem className="flex">
+            <SvgIcon
+              as="a"
+              href="https://www.facebook.com/bogdan.omelianets"
+              title="Facebook"
+            >
+              <FacebookSVG />
+            </SvgIcon>
+            <SvgIcon
+              as="a"
+              href="https://www.instagram.com/bohdan_omelianets/"
+              title="Instagram"
+            >
+              <InstagramSVG />
+            </SvgIcon>
+            <SvgIcon
+              as="a"
+              href="https://www.linkedin.com/in/bohdan-omelianets-4926a821b/"
+              title="LinkedIn"
+            >
+              <LinkedinSVG />
+            </SvgIcon>
+          </ContactItem>
+        </DivStyled>
       </Content>
-      
     </Container>
   );
 };
